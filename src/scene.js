ae416d582ef0293e3898d241dbef464c9dacfcb8
點 <sh4o4hs@gmail.com>
2021-03-19 11:19:12 +0800

    update

--- src/scene.js
+++ src/scene.js
@@ -17,6 +17,29 @@ import * as strings from 'language/strings';
 let eventList = null;
 
 
+export async function getLogo () {
+  let game = app.game;
+  let setting = app.setting;
+  let obj = await game.getProject('video/photos');
+  let pathname = obj.pathname;
+  let filename = await obj.lib.getLogo(setting.agent);
+  filename = `${pathname}/${filename}`;
+  console.log(filename);
+  return new Promise((resolve, reject) => {
+    const loader = new PIXI.Loader(); // you can also create your own if you want
+    loader.add('logo', filename);
+    loader.load((loader, resources) => {
+      console.log('resources: ', resources);
+      if (resources.logo && resources.logo.texture) {
+        resolve(resources.logo.texture);
+      } else {
+        reject(null);
+      }
+    });
+  });
+}
+
+
 /**
  * 初始化事件 (接收大廳傳送的命令用)
  * @returns {Object} 傳回物件事件
@@ -31,6 +54,7 @@ export async function init (config) {
   app.isChild = config.isChild;
   app.game = config.game;
   app.nuts = config.nuts;
+  app.setting = config.setting;
 
   app.baseURL = baseURL;
   app.langID = langID;
@@ -46,7 +70,8 @@ export async function init (config) {
      * 建立場景
      * @param conf config
      */
-    async create (/* conf */)  {
+    async create (conf)  {
+      console.log(conf);
       let game = app.game;
       let loadingEvent = game.scene.loadingEvent;
 
@@ -98,6 +123,15 @@ export async function init (config) {
       /// 傳送網路命令
       let cmd = await import('net/command/create');
       cmd.send();
+
+      getLogo().then(texture => {
+        let sprite = new PIXI.Sprite(texture);
+        sprite.x = 100;
+        sprite.y = 100;
+        sprite.anchor.x = 0.5;
+        sprite.anchor.y = 0.5;
+        game.layer.foreground.addChild(sprite);
+      });
 
       game.disconnect = () => {
         console.log('!!!! game.disconnect !!!!');
